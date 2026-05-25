9e55b42977692ca85389e9dedcf79ec18f4175d1
AashrikaTimsina <162945241+AashrikaTimsina@users.noreply.github.com>
2026-05-25 12:01:14 +0545

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello! My first Python server is not running. Shetbro")
+        self.wfile.write(b"Hello! My first Python server is not running. Shetbro, OH no bro")
 
 HTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
